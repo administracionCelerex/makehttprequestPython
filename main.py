f41726fb1f324bc7483cb9bff1a02fc07b2835bb
luissevilla <luissevilla111@gmail.com>
2022-09-27 10:31:15 -0500

    first try sending request

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
-def sendHttpRequest(url:str,method:str):
+import json
+import httplib2
+
+def sendHttpRequest(url:str,method:str,body:dict)->str:
     print("Hi")
-    pass
+    http = httplib2.Http()
+    try:
+        response= http.request(url,method=method,body=json.dumps(body))
+        print(response[1])
+    except Exception as e:
+        print("Error on the server")
+        print(e)
+    
+
+sendHttpRequest("https://jsdhfjsdf",method="POST",body=23)
 
 
-print(sendHttpRequest(url="",method=""))
     
 
 
